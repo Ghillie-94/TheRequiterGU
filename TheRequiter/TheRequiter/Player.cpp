fd1f3b2a39007b0441fe890cf7d8dcbd83d7f16d
cameron boyle <cameronboyle7894@gmail.com>
2023-06-12 18:24:42 +0100

    defined update function (added player physics)

--- TheRequiter/TheRequiter/Player.cpp
+++ TheRequiter/TheRequiter/Player.cpp
@@ -1,1 +1,140 @@
 #include "Player.h"
+#include "AssetManager.h"
+
+enum class PhysicsType
+{
+	FORWARD_EULER,
+	BACKWARD_EULER,
+	SYMPLECTIC_EULER,
+	POSITION_VERLET,
+	VELOCITY_VERLET
+};
+
+Player::Player()
+	: SpriteObject()
+	, twoFramesOldPos(100, 100)
+	, velocity(0, 0)
+	, acceleration(0, 0)
+{
+	sprite.setTexture(AssetManager::RequestTexture("Assets/Graphics/Frank/Idle1.png"));
+	collisionOffset = sf::Vector2f(0, 30);
+	collisionScale = sf::Vector2f(0.5f, 0.5f);
+	collisionType = CollisionType::AABB;
+}
+
+void Player::Update(sf::Time frameTime)
+{
+	const float DRAG_MULT = 1.0f;
+	const PhysicsType physics = PhysicsType::FORWARD_EULER;
+
+	switch (physics)
+	{
+	case PhysicsType::FORWARD_EULER:
+	{
+		// EXPLICIT EULER (FORWARD EULER)
+		SetPosition(GetPosition() + velocity * frameTime.asSeconds());
+		velocity = velocity + acceleration * frameTime.asSeconds();
+
+		// drag
+		velocity.x = velocity.x - velocity.x * DRAG_MULT * frameTime.asSeconds();
+
+		// Update acceleration
+		UpdateAcceleration();
+	}
+	break;
+
+	case PhysicsType::BACKWARD_EULER:
+	{
+		// IMPLICIT / BACKWARD EULER
+
+		// Update acceleration
+		UpdateAcceleration();
+
+		velocity = velocity + acceleration * frameTime.asSeconds();
+
+		// drag
+		velocity.x = velocity.x - velocity.x * DRAG_MULT * frameTime.asSeconds();
+
+		SetPosition(GetPosition() + velocity * frameTime.asSeconds());
+
+	}
+	break;
+
+	case PhysicsType::SYMPLECTIC_EULER:
+	{
+		// SEMI-IMPLICIT / SYMPLECTIC_EULER
+
+		velocity = velocity + acceleration * frameTime.asSeconds();
+
+		// drag
+		velocity = velocity - velocity * DRAG_MULT * frameTime.asSeconds();
+
+		SetPosition(GetPosition() + velocity * frameTime.asSeconds());
+
+		// Update acceleration
+		UpdateAcceleration();
+	}
+	break;
+
+	case PhysicsType::POSITION_VERLET:
+	{
+		// Update acceleration
+		UpdateAcceleration();
+
+		sf::Vector2f lastFramePos = GetPosition();
+
+		// Calculate current fram's position
+		SetPosition(2.0f * lastFramePos - twoFramesOldPos + acceleration * frameTime.asSeconds() * frameTime.asSeconds());
+
+		// Two frames ago (for next frame)
+		twoFramesOldPos = lastFramePos;
+	}
+	break;
+
+	case PhysicsType::VELOCITY_VERLET:
+	{
+		// VELOCITY VERLET / LEAP FROG
+
+		// Get half frame velocity using
+		// previous frame's acceleration
+		sf::Vector2f halfFrameVelocity = velocity + acceleration * frameTime.asSeconds() / 2.0f;
+
+		// Get new frame's position using half frame velocity
+		SetPosition(GetPosition() + halfFrameVelocity * frameTime.asSeconds());
+
+		// Update our current acceleration
+		UpdateAcceleration();
+
+		// Get new frame's velocity using half frame velocity and
+		// updated acceleration
+		velocity = halfFrameVelocity + acceleration * frameTime.asSeconds() / 2.0f;
+
+		// drag
+		velocity.x = velocity.x - velocity.x * DRAG_MULT * frameTime.asSeconds();
+	}
+	break;
+
+	default:
+		break;
+	}
+}
+
+void Player::HandleCollision(SpriteObject& other)
+{
+}
+
+void Player::AttackCheck()
+{
+}
+
+void Player::ChangeHealth()
+{
+}
+
+void Player::GetVelocity()
+{
+}
+
+void Player::UpdateAcceleration()
+{
+}
--- TheRequiter/TheRequiter/Player.h
+++ TheRequiter/TheRequiter/Player.h
@@ -3,5 +3,26 @@
 class Player :
     public SpriteObject
 {
+
+public:
+
+    Player();
+    void Update(sf::Time frameTime) override;
+    void HandleCollision(SpriteObject& other) override;
+    void AttackCheck();
+    void ChangeHealth();
+    void GetVelocity();
+
+private:
+
+    void UpdateAcceleration();
+
+    sf::Vector2f twoFramesOldPos;
+    sf::Vector2f velocity;
+    sf::Vector2f acceleration;
+
+    bool hasAttacked;
+    int health;
+    bool hasMovedRight;
 };
 
