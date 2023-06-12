ba5de2171eef4b14d1418798733187f9270564c8
cameron boyle <cameronboyle7894@gmail.com>
2023-06-12 15:26:05 +0100

    Defined win panel

--- TheRequiter/TheRequiter/WinPanel.cpp
+++ TheRequiter/TheRequiter/WinPanel.cpp
@@ -1,1 +1,98 @@
 #include "WinPanel.h"
+#include "AssetManager.h"
+#include "Easing.h"
+
+WinPanel::WinPanel(sf::RenderWindow* newWindow)
+	: background()
+	, title()
+	, message()
+	, position(0,0)
+	, window(newWindow)
+	, animatingIn(false)
+	, animationClock()
+{
+	background.setTexture(AssetManager::RequestTexture("insertgraphicforpanelhere"));
+	background.setScale(5.0f, 5.0f);
+
+	title.setFont(AssetManager::RequestFont("Assets/Fonts/good-times.rg-regular.otf"));
+	title.setCharacterSize(70);
+	title.setString("YOU WIN!");
+	title.setFillColor(sf::Color::Magenta);
+	title.setOutlineColor(sf::Color::Cyan);
+
+	message.setFont(AssetManager::RequestFont("Assets/Fonts/good-times.rg-regular.otf"));
+	message.setCharacterSize(30);
+	message.setString("MENU");
+	message.setFillColor(sf::Color::Magenta);
+	message.setOutlineColor(sf::Color::Cyan);
+
+	ResetPosition();
+}
+
+void WinPanel::Update(sf::Time frameTime)
+{
+
+	if (animatingIn)
+	{
+		float xPos = window->getSize().x * 0.5f - background.getGlobalBounds().width * 0.5f;
+		float yPos = window->getSize().y;
+		float finalYPos = window->getSize().y * 0.5f - background.getGlobalBounds().height * 0.5f;
+
+		sf::Vector2f begin(xPos, yPos);
+		sf::Vector2f change(0, finalYPos - yPos);
+		float duration = 1.0f;
+		float time = animationClock.getElapsedTime().asSeconds();
+
+		sf::Vector2f newPosition = Easing::EaseInQuad(begin, change, duration, time);
+		SetPosition(newPosition);
+
+		if (time >= duration)
+		{
+			SetPosition(begin + change);
+			animatingIn = false;
+		}
+	}
+
+}
+
+void WinPanel::Draw(sf::RenderTarget& target)
+{
+
+	target.draw(background);
+	target.draw(message);
+	target.draw(title);
+
+}
+
+void WinPanel::SetPosition(sf::Vector2f newPosition)
+{
+
+	background.setPosition(newPosition);
+
+	// center the title on the x direction
+	float titleX = background.getGlobalBounds().width * 0.5f - title.getGlobalBounds().width * 0.5f;
+	title.setPosition(sf::Vector2f(newPosition.x + titleX, newPosition.y + 50));
+
+	// center the message in the x and y direction
+	float messageX = background.getGlobalBounds().width * 0.5f - message.getGlobalBounds().width * 0.5f;
+	float messageY = background.getGlobalBounds().height * 0.5f - message.getGlobalBounds().height * 0.5f;
+	message.setPosition(sf::Vector2f(newPosition.x + messageX, newPosition.y + messageY));
+
+}
+
+void WinPanel::StartAnimation()
+{
+
+	animatingIn = true;
+	animationClock.restart();
+
+}
+
+void WinPanel::ResetPosition()
+{
+
+	float xPos = window->getSize().x * 0.5f - background.getGlobalBounds().width * 0.5f;
+	float yPos = window->getSize().y;
+	SetPosition(sf::Vector2f(xPos, yPos));
+
+}
--- TheRequiter/TheRequiter/WinPanel.h
+++ TheRequiter/TheRequiter/WinPanel.h
@@ -1,5 +1,27 @@
 #pragma once
+#include <SFML/Graphics.hpp>
+
 class WinPanel
 {
+public:
+
+	WinPanel(sf::RenderWindow* newWindow);
+
+	void Update(sf::Time frameTime);
+
+	void Draw(sf::RenderTarget& target);
+	void SetPosition(sf::Vector2f newPosition);
+	void StartAnimation();
+	void ResetPosition();
+
+private:
+	sf::Sprite background;
+	sf::Text title;
+	sf::Text message;
+	sf::Vector2f position;
+	sf::RenderWindow* window;
+
+	bool animatingIn;
+	sf::Clock animationClock;
 };
 
