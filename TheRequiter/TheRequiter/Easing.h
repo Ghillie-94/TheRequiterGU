fc48e8c7b2cb0c9ad42c5e63172ef44a63738942
cameron boyle <cameronboyle7894@gmail.com>
2023-06-12 14:42:08 +0100

    defined easing

--- TheRequiter/TheRequiter/Easing.h
+++ TheRequiter/TheRequiter/Easing.h
@@ -1,5 +1,10 @@
 #pragma once
-class Easing
+#include <SFML/Graphics.hpp>
+
+namespace Easing
 {
+	sf::Vector2f Linear(sf::Vector2f begin, sf::Vector2f change, float duration, float time);
+
+	sf::Vector2f EaseInQuad(sf::Vector2f begin, sf::Vector2f change, float duration, float time);
 };
 
