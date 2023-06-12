07a97c16e4623334a46fe4da65452567fe70b990
cameron boyle <cameronboyle7894@gmail.com>
2023-06-12 11:10:42 +0100

    Created header file for sprite object class

---
+++ TheRequiter/TheRequiter/SpriteObject.h
@@ -0,0 +1,48 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+enum class CollisionType
+{
+	CIRCLE,
+	AABB
+};
+
+class SpriteObject
+{
+
+public:
+
+	SpriteObject();
+
+	virtual void Update(sf::Time frameTime);
+	virtual void Draw(sf::RenderTarget& target);
+
+	sf::Vector2f GetPosition();
+	virtual void SetPosition(sf::Vector2f newPosition);
+	void SetPosition(float newX, float newY);
+
+	bool CheckCollision(SpriteObject other);
+	void SetColliding(bool newColliding);
+
+	sf::Vector2f GetCollisionDepth(SpriteObject other);
+	virtual void HandleCollision(SpriteObject& other);
+	void SetAlive(bool newAlive);
+
+protected:
+
+	sf::Sprite sprite;
+	sf::Vector2f collisionOffset;
+	sf::Vector2f collisionScale;
+	CollisionType collisionType;
+	bool alive;
+
+private:
+
+	sf::Vector2f GetCollisionCentre();
+	float GetCircleColliderRadius();
+	sf::FloatRect GetAABB();
+	sf::Vector2f position;
+	bool colliding;
+
+};
+
