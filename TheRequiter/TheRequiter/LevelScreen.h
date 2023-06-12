f9ca93022fe70c2cf0a6a171eb91cf87c8882854
cameron boyle <cameronboyle7894@gmail.com>
2023-06-12 09:50:39 +0100

    created level screen class

---
+++ TheRequiter/TheRequiter/LevelScreen.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "Screen.h"
+#include <SFML/Graphics.hpp>
+
+class Game;
+
+class LevelScreen :
+    public Screen
+{
+
+public:
+
+    LevelScreen(Game* newGamePointer);
+
+    void Update(sf::Time frameTime) override;
+    void Draw(sf::RenderTarget& target) override;
+    
+
+private:
+
+    void Restart();
+
+    bool isTitleScreen;
+    bool isBossAlive;
+
+};
+
