eaa250049247923c24b09fa6e965bd0373f18e1b
cameron boyle <cameronboyle7894@gmail.com>
2023-06-14 23:19:53 +0100

    declared functions and data for boss and created constructor

--- TheRequiter/TheRequiter/Boss.cpp
+++ TheRequiter/TheRequiter/Boss.cpp
@@ -1,1 +1,27 @@
 #include "Boss.h"
+#include "AssetManager.h"
+#include "VectorHelper.h"
+
+Boss::Boss(sf::Vector2f newPosition, sf::Vector2f newPos1, sf::Vector2f newPos2, Player* newPlayerPtr)
+	:Enemy(newPosition, newPlayerPtr)
+	, health(180)
+	, canAttack(false)
+	, hasAttacked(false)
+	, playerInRange(false)
+	, SPEED(80)
+	, POS1(newPos1)
+	, POS2(newPos2)
+	, targetPoint(&POS2)
+	, velocity(0, 0)
+	, playerPtr(newPlayerPtr)
+{
+
+	sf::Clock cooldownClock;
+	sf::Time cooldownTimer;
+
+	// Update velocity
+	sf::Vector2f vectorToNewTarget = *targetPoint - GetPosition();
+	vectorToNewTarget = VectorHelper::Normalise(vectorToNewTarget);
+	velocity = vectorToNewTarget * SPEED;
+
+}
--- TheRequiter/TheRequiter/Boss.h
+++ TheRequiter/TheRequiter/Boss.h
@@ -3,5 +3,42 @@
 class Boss :
     public Enemy
 {
+public:
+
+    Boss(sf::Vector2f newPosition, sf::Vector2f newPos1, sf::Vector2f newPos2, Player* newPlayerPtr);
+
+    void Update(sf::Time frameTime) override;
+
+    void SetPosition(sf::Vector2f newPosition) override;
+
+    void CheckHealth();
+
+    void ChangeHealth(int damage) override;
+    void AttackTimer() override;
+    void DoAttack() override;
+    void AttackCheck() override;
+    void CheckDistance(Player* newPlayerPtr);
+
+    void SetCanAttack(bool newCanAttack);
+    void SetHasAttacked(bool newHasAttacked);
+
+
+    void CheckRange(Player* newPlayerPtr);
+
+private:
+
+    int health;
+    bool canAttack;
+    bool hasAttacked;
+    bool playerInRange;
+    const float SPEED;
+    const sf::Vector2f POS1;
+    const sf::Vector2f POS2;
+    sf::Vector2f playerPos;
+    const sf::Vector2f* targetPoint;
+    sf::Vector2f velocity;
+    Player* playerPtr;
+    sf::Clock cooldownClock;
+    sf::Time cooldownTimer;
 };
 
