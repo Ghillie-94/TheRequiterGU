ad18483337b5c6e28e9f17272e281d3489a6505e
cameron boyle <cameronboyle7894@gmail.com>
2023-06-14 10:19:47 +0100

    added slim enemy constructor

--- TheRequiter/TheRequiter/Slim.cpp
+++ TheRequiter/TheRequiter/Slim.cpp
@@ -1,1 +1,21 @@
 #include "Slim.h"
+
+
+
+Slim::Slim(sf::Vector2f newPosition, sf::Vector2f newPos1, sf::Vector2f newPos2)
+	:Enemy(newPosition)
+	, SPEED(80)
+	, POS1(newPos1)
+	, POS2(newPos2)
+	, targetPoint(&POS2)
+	, velocity(0, 0)
+{
+}
+
+void Slim::Update(sf::Time frameTime)
+{
+}
+
+void Slim::SetPosition(sf::Vector2f newPosition)
+{
+}
--- TheRequiter/TheRequiter/Slim.h
+++ TheRequiter/TheRequiter/Slim.h
@@ -3,5 +3,21 @@
 class Slim :
     public Enemy
 {
+
+public:
+
+    Slim(sf::Vector2f newPosition, sf::Vector2f newPos1, sf::Vector2f newPos2);
+
+    void Update(sf::Time frameTime) override;
+
+    void SetPosition(sf::Vector2f newPosition) override;
+
+private:
+    const float SPEED;
+    const sf::Vector2f POS1;
+    const sf::Vector2f POS2;
+    sf::Vector2f playerPos;
+    const sf::Vector2f* targetPoint;
+    sf::Vector2f velocity;
 };
 
