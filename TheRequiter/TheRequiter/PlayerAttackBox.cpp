579ca3309d79869bccab36bff2e7c89377303d87
cameron boyle <cameronboyle7894@gmail.com>
2023-06-14 07:23:07 +0100

    Player Attack box constructor implemented

--- TheRequiter/TheRequiter/PlayerAttackBox.cpp
+++ TheRequiter/TheRequiter/PlayerAttackBox.cpp
@@ -1,1 +1,9 @@
 #include "PlayerAttackBox.h"
+
+PlayerAttackBox::PlayerAttackBox()
+	:SpriteObject()
+{
+	collisionOffset = sf::Vector2f(50, 0);
+	collisionScale = sf::Vector2f(0.5f, 0.5f);
+	collisionType = CollisionType::AABB;
+}
--- TheRequiter/TheRequiter/PlayerAttackBox.h
+++ TheRequiter/TheRequiter/PlayerAttackBox.h
@@ -1,7 +1,10 @@
 #pragma once
 #include "SpriteObject.h"
+#include <SFML/Graphics.hpp>
 class PlayerAttackBox :
     public SpriteObject
 {
+public:
+    PlayerAttackBox();
 };
 
