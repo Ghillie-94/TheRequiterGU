23fb15991f531796e01c51ef00fe6654269f1227
cameron boyle <cameronboyle7894@gmail.com>
2023-06-15 05:04:53 +0100

    added level screen constructor

--- TheRequiter/TheRequiter/LevelScreen.cpp
+++ TheRequiter/TheRequiter/LevelScreen.cpp
@@ -1,6 +1,14 @@
 #include "LevelScreen.h"
 
 LevelScreen::LevelScreen(Game* newGamePointer)
+	: Screen(newGamePointer)
+	, isTitleScreen(true)
+	, isBossAlive(true)
+	, isPlayerAlive(true)
+	, player(nullptr)
+	, winPanel(newGamePointer->GetWindow())
+	, losePanel(newGamePointer->GetWindow())
 
 {
+	//add restart here
 }
