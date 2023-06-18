12a11fc0adeacb8be6c354a70154ed7a98014d68
cameron boyle <cameronboyle7894@gmail.com>
2023-06-18 20:37:08 +0100

    defined update func on parallax class

--- TheRequiter/TheRequiter/PierFerrisLayer.cpp
+++ TheRequiter/TheRequiter/PierFerrisLayer.cpp
@@ -1,1 +1,24 @@
 #include "PierFerrisLayer.h"
+
+PierFerrisLayer::PierFerrisLayer(sf::Vector2f newPosition, Player* newPlayerPtr)
+	: Parallax()
+	, playerPtr(newPlayerPtr)
+	, playerVelocity(0)
+	, SCROLLSPEED(50)
+{
+	sprite.setTexture(AssetManager::RequestTexture("Assets/Graphics/Parallax/PierFerrisLayer.png"));
+	SetPosition(newPosition);
+}
+
+void PierFerrisLayer::Update(sf::Time frameTime)
+{
+	playerVelocity = playerPtr->GetVelocity().x;
+	if (playerVelocity > 0)
+	{
+		this->sprite.move(SCROLLSPEED, 0);
+	}
+	else if (playerVelocity < 0)
+	{
+		this->sprite.move(-SCROLLSPEED, 0);
+	}
+}
--- TheRequiter/TheRequiter/PierFerrisLayer.h
+++ TheRequiter/TheRequiter/PierFerrisLayer.h
@@ -1,7 +1,17 @@
 #pragma once
 #include "Parallax.h"
+#include "Player.h"
 class PierFerrisLayer :
     public Parallax
 {
+public:
+    PierFerrisLayer(sf::Vector2f newPosition, Player* newPlayerPtr);
+
+    void Update(sf::Time frameTime) override;
+
+private:
+    Player* playerPtr;
+    float playerVelocity;
+    const float SCROLLSPEED;
 };
 
