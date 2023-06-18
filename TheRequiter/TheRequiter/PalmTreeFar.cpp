d7032b21ef2ad78745ee7e08d4380b96a097a203
cameron boyle <cameronboyle7894@gmail.com>
2023-06-18 20:51:58 +0100

    declared and defined constructor, functions, & data on palm tree far class

--- TheRequiter/TheRequiter/PalmTreeFar.cpp
+++ TheRequiter/TheRequiter/PalmTreeFar.cpp
@@ -1,1 +1,24 @@
 #include "PalmTreeFar.h"
+
+PalmTreeFar::PalmTreeFar(sf::Vector2f newPosition, Player* newPlayerPtr)
+	: Parallax()
+	, playerPtr(newPlayerPtr)
+	, playerVelocity(0)
+	, SCROLLSPEED(30)
+{
+	sprite.setTexture(AssetManager::RequestTexture("Assets/Graphics/Parallax/PalmTreeFar.png"));
+	SetPosition(newPosition);
+}
+
+void PalmTreeFar::Update(sf::Time frameTime)
+{
+	playerVelocity = playerPtr->GetVelocity().x;
+	if (playerVelocity > 0)
+	{
+		this->sprite.move(SCROLLSPEED, 0);
+	}
+	else if (playerVelocity < 0)
+	{
+		this->sprite.move(-SCROLLSPEED, 0);
+	}
+}
--- TheRequiter/TheRequiter/PalmTreeFar.h
+++ TheRequiter/TheRequiter/PalmTreeFar.h
@@ -1,7 +1,18 @@
 #pragma once
 #include "Parallax.h"
+#include "Player.h"
 class PalmTreeFar :
     public Parallax
 {
+public:
+    PalmTreeFar(sf::Vector2f newPosition, Player* newPlayerPtr);
+
+    void Update(sf::Time frameTime) override;
+
+private:
+
+    Player* playerPtr;
+    float playerVelocity;
+    const float SCROLLSPEED;
 };
 
