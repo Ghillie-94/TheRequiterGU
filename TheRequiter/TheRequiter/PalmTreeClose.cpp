fe38b87b33f73554235c2e213961309eb6054da1
cameron boyle <cameronboyle7894@gmail.com>
2023-06-18 21:01:59 +0100

    declared and defined constructor, functions, and data on palm tree close class

--- TheRequiter/TheRequiter/PalmTreeClose.cpp
+++ TheRequiter/TheRequiter/PalmTreeClose.cpp
@@ -1,1 +1,24 @@
 #include "PalmTreeClose.h"
+
+PalmTreeClose::PalmTreeClose(sf::Vector2f newPosition, Player* newPlayerPtr)
+	: Parallax()
+	, playerPtr(newPlayerPtr)
+	, playerVelocity(0)
+	, SCROLLSPEED(70)
+{
+	sprite.setTexture(AssetManager::RequestTexture("Assets/Graphics/Parallax/PierFerrisLayer.png"));
+	SetPosition(newPosition);
+}
+
+void PalmTreeClose::Update(sf::Time frameTime)
+{
+	playerVelocity = playerPtr->GetVelocity().x;
+	if (playerVelocity > 0)
+	{
+		this->sprite.move(SCROLLSPEED, 0);
+	}
+	else if (playerVelocity < 0)
+	{
+		this->sprite.move(-SCROLLSPEED, 0);
+	}
+}
--- TheRequiter/TheRequiter/PalmTreeClose.h
+++ TheRequiter/TheRequiter/PalmTreeClose.h
@@ -1,7 +1,19 @@
 #pragma once
 #include "Parallax.h"
+#include "Player.h"
 class PalmTreeClose :
     public Parallax
 {
+public:
+
+    PalmTreeClose(sf::Vector2f newPosition, Player* newPlayerPtr);
+
+    void Update(sf::Time frameTime) override;
+
+private:
+
+    Player* playerPtr;
+    float playerVelocity;
+    const float SCROLLSPEED;
 };
 
